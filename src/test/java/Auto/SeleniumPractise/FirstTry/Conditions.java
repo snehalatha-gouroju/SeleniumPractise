23d312b88039b30f36e084c50054cb438caeadbf
snehalatha <snehagouroju804@gmail.com>
2026-06-14 09:52:45 +0530

    fourth commit

--- src/test/java/Auto/SeleniumPractise/FirstTry/Conditions.java
+++ src/test/java/Auto/SeleniumPractise/FirstTry/Conditions.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+///FOURTH COMMIT
 public class Conditions {
 	static WebDriver driver;
 

fd1f59f83eb2a556b9ccff5b50707415a36f44b3
snehalatha <snehagouroju804@gmail.com>
2026-06-16 22:57:20 +0530

    fifth commit

--- src/test/java/Auto/SeleniumPractise/FirstTry/ClickableWaiting.java
+++ src/test/java/Auto/SeleniumPractise/FirstTry/ClickableWaiting.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+//clickable 
 public class ClickableWaiting{
 	static WebDriver driver;
 
